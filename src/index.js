837a0d762a3733ffc4d26e07b83a22c3c505c295
nasim1717 <98159877+nasim1717@users.noreply.github.com>
2024-12-28 13:30:25 +0600

    home and quote page responsive

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 // Add scroll event listener to change navbar style
 const navbar = document.getElementById("navbar");
+const menuButtonOpen = document.getElementById("menu-button-open");
+const menuButtonClose = document.getElementById("menu-button-close");
+const navLink = document.getElementById("nav-link");
+const loginButton = document.getElementById("login-button");
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
@@ -11,3 +15,22 @@ window.addEventListener("scroll", () => {
         navbar.classList.add("bg-gray-600/10");
     }
 });
+
+function menuOpenFunction(e) {
+    menuButtonOpen.classList.add("hidden");
+    menuButtonClose.classList.remove("hidden")
+    menuButtonClose.classList.add("block");
+    loginButton.classList.remove("hidden");
+    loginButton.classList.add("block");
+    navLink.classList.remove("hidden");
+
+}
+function menuCloseFunction(e) {
+    menuButtonOpen.classList.remove("hidden");
+    menuButtonOpen.classList.add("block");
+    menuButtonClose.classList.add("hidden");
+    navLink.classList.add("hidden");
+    loginButton.classList.add("hidden");
+    loginButton.classList.remove("block");
+
+}
